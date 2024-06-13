9244ee263f05edba645eab0438bdf49667fd8e86
mudotet <mudotet@example.com>
2024-06-13 16:46:35 +0700

    four commit

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
     $severname = "localhost";
     $username = "root";
     $password = "";
-    $db = "pheufood";   
+    $db = "pheu_hot_pot";   
     $conn =new mysqli($severname,$username,$password,$db);
     if ($conn -> connect_error){
         dia("Connection failed:".$conn -> connect_error);
